4281323e38da8e3a51a0726848be6620b681e2db
junco <junco3@gmail.com>
2019-11-14 13:42:13 -0600

    Added more comments. Rearanged __main__.

--- webserver.py
+++ webserver.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
-"""
-Very simple HTTP server in python for logging requests
-Usage::
-    ./server.py [<port>]
-"""
+
+# This is an include statement. It tells python that it should load up
+# the functions for HTTPServer and logging because we're going to use them
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 
-class S(BaseHTTPRequestHandler):
-    def _set_response(self):
+##################
+###  SETTINGS  ###
+##################
+
+# The port is the part put after the colon in the URL. Example  localhost:8080/pictures
+# The browser will default to 80 if you don't put one in the URL
+port=8080
+
+#####################
+###  DEFINITIONS  ###
+#####################
+
+class Request_handler(BaseHTTPRequestHandler):
+    def set_200_response_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
+        self.set_200_response_headers()
         self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
     def do_POST(self):
@@ -26,23 +36,20 @@ class S(BaseHTTPRequestHandler):
 
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        
+##############
+###  MAIN  ###
+##############
 
-def run(server_class=HTTPServer, handler_class=S, port=8080):
+# This is the 'main' part of the program. When you start the program this is the first thing that gets run.
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
+    httpd = HTTPServer(server_address, Request_handler)
+    logging.info('Starting webserver on localhost:{} ...\n'.format(port))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
     logging.info('Stopping httpd...\n')
-
-if __name__ == '__main__':
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
