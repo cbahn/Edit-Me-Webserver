89f33a7117bcc7f42cfc0eeaba73ef441ef68f68
junco <junco3@gmail.com>
2019-11-15 10:07:55 -0600

    Added comments and info about logging

--- webserver.py
+++ webserver.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Based Largely off of https://gist.github.com/mdonkers/63e115cc0c79b4f6b8b3a6b797e485c7
+
 # This is an include statement. It tells python that it should load up
 # the functions for HTTPServer, Template, and logging because we're going to use them
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -13,6 +15,13 @@ import logging
 # The port is the part put after the colon in the URL. Example  localhost:8080/pictures
 # The browser will default to 80 otherwise
 port=8080
+
+# This determines how much info is displayed to the console window after each request
+#  - logging.INFO      every request will be logged
+#  - logging.WARNING   only requests that fail and errors will be logged
+#  - logging.CRITICAL  no logs except if the program crashes
+logging_level = logging.INFO
+
 
 #####################
 ###  DEFINITIONS  ###
@@ -37,12 +46,12 @@ def page_builder():
 
 
 class Request_handler(BaseHTTPRequestHandler):
-    def set_200_response_headers(self):
+    def _set_200_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
     
-    def set_400_response(self):
+    def _set_400_response(self):
         self.send_response(400)
         self.end_headers()
 
@@ -53,12 +62,12 @@ class Request_handler(BaseHTTPRequestHandler):
         # Also, the post request should start redirecting correctly
         
         if self.path == '/':
-            self.set_200_response_headers()
+            self._set_200_response()
             self.wfile.write(page_builder().encode('utf-8'))
         elif self.path == '/res/style.css':
             pass
         else:
-            self.set_400_response()
+            self._set_400_response()
 
 # wikipedia.org/wiki/Post/Redirect/Get
 
@@ -68,22 +77,23 @@ class Request_handler(BaseHTTPRequestHandler):
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
 
-        self.set_200_response_headers()
+        self._set_200_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
         
 ##############
 ###  MAIN  ###
 ##############
+# This is the 'main' part of the program. It is the first thing run when the program is started
 
-# This is the 'main' part of the program. When you start the program this is the first thing that gets run.
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    # Turn on logging so that info appears in the console window
+    logging.basicConfig(level=logging_level)
+    
+    # Start the webserver listening at our home address and port
+    # Request_handler will be responsible for all requests
     server_address = ('', port)
     httpd = HTTPServer(server_address, Request_handler)
     logging.info('Starting webserver on localhost:{} ...\n'.format(port))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+
+    # For now the only way to exit the server is by ending the process
+    httpd.serve_forever()
