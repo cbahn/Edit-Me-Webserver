a2c8131f52feaf377a4f6ac5423ce2bff2ac365c
junco <junco3@gmail.com>
2019-11-14 22:59:30 -0600

    Modified GET handler so that it matches the path exactly, also started handling POST requests

--- webserver.py
+++ webserver.py
@@ -41,20 +41,34 @@ class Request_handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+    
+    def set_400_response(self):
+        self.send_response(400)
+        self.end_headers()
 
     def do_GET(self):
         logging.info("GET request,\nPath: %s\n", str(self.path))
-        self.set_200_response_headers()
-        self.wfile.write(page_builder().encode('utf-8'))
-        #self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        
+        ## NEXT STEPS: add stuff to load the res properly
+        # Also, the post request should start redirecting correctly
+        
+        if self.path == '/':
+            self.set_200_response_headers()
+            self.wfile.write(page_builder().encode('utf-8'))
+        elif self.path == '/res/style.css':
+            pass
+        else:
+            self.set_400_response()
+
+# wikipedia.org/wiki/Post/Redirect/Get
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
 
-        self._set_response()
+        self.set_200_response_headers()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
         
 ##############
